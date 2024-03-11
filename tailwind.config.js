912c91d102ff17ba43b9a0e09ad1a1ed1b497ea1
parham <parhamusobii@gmail.com>
2024-03-11 16:15:22 +0330

    Wischool

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode:'class',
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '2.5rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+  },
+  plugins: [],
+}
